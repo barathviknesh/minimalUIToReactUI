002a206c96b23a32717330da690cb531325f407a
barathviknesh <57832162+barathviknesh@users.noreply.github.com>
2022-03-10 12:57:34 +0530

    Update index.js

--- src/components/cardTypeTwo/index.js
+++ src/components/cardTypeTwo/index.js
@@ -1,12 +1,66 @@
-import { Paper } from '@mui/material';
-
-
+import { Paper, Typography } from '@mui/material';
+import Chart from "react-apexcharts";
+import InsightsOutlinedIcon from '@mui/icons-material/InsightsOutlined';
 
 
 
 export const CardTypTwo = props => {
 
 
+    let ChartData = {
+        series: [{
+            name: '$',
+            data: [11, 32, 45, 32, 34, 52, 41],
+        }],
+        options: {
+            colors: ['#00ab55'],
+            chart: {
+                stacked: true,
+                type: 'area',
+                toolbar: {
+                    show: false
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                curve: 'smooth'
+            },
+            xaxis: {
+                show: false,
+                labels: { show: false },
+                tooltip: {
+                    enabled: false
+                }
+            },
+            tooltip: {
+                x: {
+                    show: false
+                },
+                y: {
+                    show: false
+                }
+            },
+            yaxis: {
+                show: false,
+                labels: { show: false }
+            },
+            legend: {
+                show: false
+            },
+            grid: {
+                show: false
+            },
+            fill: {
+                type: 'gradient',
+                gradient: {
+                    opacityFrom: 0,
+                    opacityTo: 0
+                }
+            }
+        }
+    }
 
     return (
         <Paper style={{
@@ -17,20 +71,35 @@ export const CardTypTwo = props => {
             boxShadow: 'rgb(145 158 171 / 20%) 0px 0px 2px 0px, rgb(145 158 171 / 12%) 0px 12px 24px -4px',
         }}>
             <div style={{
-                display: "flex",
-                // flexDirection: "column",
-                justifyContent: 'center',
-                alignItems: 'center',
-                // marginLeft: "10px",
-                // padding: "15px"
+                height: "100%",
+                width: "100%",
+                overflow: 'hidden'
             }}>
-                <img src={props?.img ? props?.img : "images/total_income.png"} alt="logo" style={{
-                    height: "280px",
-                    maxWidth: "96%",
-                    "object-fit": "contain",
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <div style={{ marginLeft: "14px", marginTop: "14px" }}>
+                        <Typography variant="subtitle1" style={{ fontWeight: "600", color: "#458c76" }}>Total Income</Typography>
+                        <Typography variant="h4" style={{ fontWeight: "700", color: '#005249' }}>$18,765</Typography>
+                    </div>
+                    <div style={{ marginRight: "14px", marginTop: "18px" }}>
+                        <div style={{ display: 'flex' }}>
+                            <InsightsOutlinedIcon style={{ color: "#126155" }} />
+                            <Typography variant="p" style={{ fontWeight: "700", color: '#126155', marginLeft: "4px" }}>+2.6%</Typography>
+                        </div>
+                        <div>
+                            <Typography variant="subtitle1" style={{ color: '#70b092', marginTop: '6px' }}>than last month</Typography>
+                        </div>
 
-                }} />
+                    </div>
+                </div>
+
+                < Chart
+                    options={ChartData.options}
+                    series={ChartData.series}
+                    type="area"
+                    width="100%"
+                    height="70%"
+                />
             </div>
-        </Paper>
+        </Paper >
     )
 }
